41650539375aaf48bd4834285521f2d4bab3b3bf
Alvaro Martinez <mafermedia@gmail.com>
2020-09-10 17:08:11 +0200

    Update Experiment1.cs
    Experiment1

--- Assets/Scripts/Experiment1.cs
+++ Assets/Scripts/Experiment1.cs
@@ -7,19 +7,19 @@ using UnityEngine;
 public class Experiment1 : MonoBehaviour
 {
     public Transform network;
-    public Transform rightController;
-    public TextMesh textController;
-    public TextMesh textNetwork;
-    private string currentNode;
     public GameObject line;
     private List<GameObject> lines;
 
-    private Dictionary<string, ParticleSystem.Particle> particles;
     private IEnumerator selectNodeCoroutine;
     string[] keys;
-    private int numKeys;
     private int num;
     private bool execution;
+    private int numKeys;
+
+    private int qty = 0;
+    private float currentAvgFPS = 0;
+
+    private float[] fpsDict;
 
     // Start is called before the first frame update
     void Start()
@@ -27,37 +27,46 @@ public class Experiment1 : MonoBehaviour
         // Inizialize lines for the eges of the network
         lines = new List<GameObject>();
 
-        particles = LoadFile.particlesBlood;
-        keys = particles.Keys.ToArray();
+        keys = LoadFile.particlesBlood.Keys.ToArray();
         numKeys = keys.Length;
-
         num = 0;
 
         execution = false;
+
+        fpsDict = new float[20];
     }
 
     // Update is called once per frame
     void Update()
     {
-        //network.transform.Translate(Vector3.back * -0.35f * Time.deltaTime);
+        //network.transform.Translate(Vector3.back * -0.25f * Time.deltaTime);
 
-        if (execution == false){
+        if (execution == false && num < 19){
             selectNodeCoroutine = selectNode(keys[num], 0.5f);
             StartCoroutine(selectNodeCoroutine);
+
+            Debug.Log(num);
+
+            qty = 0;
+        }else if( num == 18)
+        {
+            string results = "";
+            int j = 1;
+            foreach(float i in fpsDict)
+            {
+                results = results + " (" + j + ", " + i + ")";
+                j++;
+            }
+            Debug.Log(results);
+            num += 1;
         }
     }
 
     private IEnumerator selectNode(string gene_string, float waitTime)
     {
-        Dictionary<string, List<string>> particle_relations = LoadFile.networkBlood;
-
         execution = true; // set the flag
 
         yield return new WaitForSeconds(waitTime);
-
-        currentNode = gene_string;
-
-        num += 1;
 
         // Haptics right controller vibration
         StartCoroutine(Haptics(0.5f, 0.5f, 0.2f, true, false));
@@ -68,25 +77,31 @@ public class Experiment1 : MonoBehaviour
         }
         lines.Clear();
 
+        int maxLines = LoadFile.networkBlood[gene_string].Count/2;
+        int currentNumLines = 0;
+
         // CPU usage - Evaluation
-        foreach (string remote_gene in particle_relations[gene_string])
+        foreach (string remote_gene in LoadFile.networkBlood[gene_string])
         {
+            if (++currentNumLines == maxLines) break;
             try
             {
                 Vector3[] vs = new Vector3[2];
                 GameObject clone;
                 LineRenderer clone_line;
 
-                vs[0] = transform.TransformPoint(particles[remote_gene].position);
-                vs[1] = transform.TransformPoint(particles[gene_string].position);
+                vs[0] = transform.TransformPoint(LoadFile.particlesBlood[remote_gene].position);
+                vs[1] = transform.TransformPoint(LoadFile.particlesBlood[gene_string].position);
 
                 clone = Instantiate(line);
-                clone.transform.parent = this.transform;
+                clone.transform.parent = network.transform;
                 clone_line = clone.GetComponent<LineRenderer>();
 
                 clone_line.SetPositions(vs);
 
                 lines.Add(clone);
+
+                fpsDict[num] = UpdateCumulativeMovingAverageFPS(1 / Time.deltaTime);
             }
             catch (InvalidCastException e)
             {
@@ -96,6 +111,8 @@ public class Experiment1 : MonoBehaviour
         }
 
         execution = false; // clear the flag before returning
+
+        num += 1;
     }
 
     private IEnumerator Haptics(float frequency, float amplitude, float duration, bool rightHand, bool leftHand)
@@ -107,5 +124,13 @@ public class Experiment1 : MonoBehaviour
 
         if (rightHand) OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
         if (leftHand) OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+    }
+
+    private float UpdateCumulativeMovingAverageFPS(float newFPS)
+    {
+        ++qty;
+        currentAvgFPS += (newFPS - currentAvgFPS) / qty;
+
+        return currentAvgFPS;
     }
 }
